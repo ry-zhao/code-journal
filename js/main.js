b878c468b1594f8513a91441458f0ded8d71cf33
Ryan Zhao <ryanzhao21@gmail.com>
2021-09-07 16:53:06 -0700

    Changed photo-url naming on line 24.

--- js/main.js
+++ js/main.js
@@ -7,9 +7,9 @@ var form = document.querySelector('form');
 function saveEntry(event) {
   event.preventDefault();
   var entry = {};
-  entry.title = form.elements.title.value;
-  entry.photoUrl = form.elements['photo-url'].value;
-  entry.notes = form.elements.notes.value;
+  entry.title = form.elements[0].value;
+  entry.photoUrl = form.elements[1].value;
+  entry.notes = form.elements[2].value;
   entry.id = data.nextEntryId;
   data.nextEntryId++;
   data.entries.unshift(entry);
@@ -21,5 +21,5 @@ function updateImg(event) {
   img.setAttribute('src', event.target.value);
 }
 
-form.elements[1].addEventListener('input', updateImg);
+form.elements['photo-url'].addEventListener('input', updateImg);
 form.addEventListener('submit', saveEntry);
